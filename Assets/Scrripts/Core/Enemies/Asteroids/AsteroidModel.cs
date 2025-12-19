8919ab23d64055e430eff700ad5aaf6c14e8d367
Vseslav <Vseslav2244@gmail.com>
2025-12-19 05:00:18 +0300

    add asteroid fragmentation data

--- Assets/Scrripts/Core/Enemies/Asteroids/AsteroidModel.cs
+++ Assets/Scrripts/Core/Enemies/Asteroids/AsteroidModel.cs
@@ -13,6 +13,40 @@ namespace Asteroids.Core.Enemies
             Size = size;
             Type = EnemyType.Asteroid;
         }
+
+        /// <summary>
+        /// Check if asteroid can fragment (when hit by bullets)
+        /// </summary>
+        public bool CanFragment()
+        {
+            return Size != AsteroidSize.Small;
+        }
+
+        /// <summary>
+        /// Get the size of fragments that should be created when this asteroid fragments
+        /// </summary>
+        public AsteroidSize GetFragmentSize()
+        {
+            return Size switch
+            {
+                AsteroidSize.Large => AsteroidSize.Medium,
+                AsteroidSize.Medium => AsteroidSize.Small,
+                _ => AsteroidSize.Small
+            };
+        }
+
+        /// <summary>
+        /// Get the number of fragments to create
+        /// </summary>
+        public int GetFragmentCount()
+        {
+            return Size switch
+            {
+                AsteroidSize.Large => 2,
+                AsteroidSize.Medium => 2,
+                _ => 0
+            };
+        }
     }
 }
 
