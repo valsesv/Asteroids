6c6add79ee9c4a5155d93c509f1114984af64f75
Vseslav <Vseslav2244@gmail.com>
2026-01-03 13:43:06 +0300

    add game background

---
+++ Assets/Scripts/Misc/TilingBackground.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Asteroids.Misc
+{
+    public class TilingBackground : MonoBehaviour
+    {
+        [SerializeField] private Renderer _renderer;
+        [SerializeField] private float _speed;
+
+        private Vector2 _offset;
+
+        private void Update()
+        {
+            _offset.y += _speed * Time.deltaTime;
+            _renderer.material.mainTextureOffset = _offset;
+        }
+    }
+}
+
